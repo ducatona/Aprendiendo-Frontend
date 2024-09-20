adc328a461a8c654bee7251268370d099c59f14e
Adrián Peña Carnero <adrianpcarnero@gmail.com>
2024-09-20 20:15:02 +0200

    no message

---
+++ DWES/EJERCICIOS/PrimerProgramaPHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Desarrollo web</title>
+</head>
+
+<body>
+    <h1>Desarrollo en servidor</h1>
+    <?php
+    phpinfo();
+    phpversion();
+
+    ?>
+</body>
+
+</html>
