8b4f5f3efa3ff914659d4017bd4c1fce2a8d4821
Adrián Peña Carnero <adrianpcarnero@gmail.com>
2024-09-25 13:34:11 +0200

    25/09/2024

--- DWES/EJERCICIOS/EjerciciosPHP/Hoja4/index.php
+++ DWES/EJERCICIOS/EjerciciosPHP/Hoja4/index.php
@@ -1,26 +1,119 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
+
 <body>
-    
+
+    <!-- Ejercicio 1 -->
+    <?php
+    // Establecer la zona horaria
+    date_default_timezone_set('Europe/Madrid');
+
+    // Obtener la fecha actual
+    $fechaActual = date('l, d \d\e F \d\e Y');
+
+    // Meses y dias de ingles a español
+    $diasSemana = [
+        'Sunday' => 'Domingo',
+        'Monday' => 'Lunes',
+        'Tuesday' => 'Martes',
+        'Wednesday' => 'Miércoles',
+        'Thursday' => 'Jueves',
+        'Friday' => 'Viernes',
+        'Saturday' => 'Sábado',
+    ];
+
+    $meses = [
+        'January' => 'Enero',
+        'February' => 'Febrero',
+        'March' => 'Marzo',
+        'April' => 'Abril',
+        'May' => 'Mayo',
+        'June' => 'Junio',
+        'July' => 'Julio',
+        'August' => 'Agosto',
+        'September' => 'Septiembre',
+        'October' => 'Octubre',
+        'November' => 'Noviembre',
+        'December' => 'Diciembre',
+    ];
+
+    // Transformación de nombres
+    foreach ($diasSemana as $en => $es) {
+        $fechaActual = str_replace($en, $es, $fechaActual);
+    }
+
+    foreach ($meses as $en => $es) {
+        $fechaActual = str_replace($en, $es, $fechaActual);
+    }
+
+    // Mostrar la fecha
+    echo "<h1>Fecha Actual</h1>";
+    echo "<p>$fechaActual</p>";
+    ?>
+
+
+
+<!--Ejercicio 2 -->
+    <?php
+
+
+    $suma = 0;
+    for ($a = 10; $a <= 100; ++$a) {
+
+        if ($a % 2 == 0) {
+            $suma += $a;
+        }
+    }
+
+print ("El resultado de la suma del 10 al 100 de los numeros pares naturales es: $suma");
+
+    ?>
+
+
+
+<!--Ejercicio 3-->
+ 
 
 <?php
 
 
-$fecha = new DateTime('2022-09-22');
-
-$formato = new IntlDateFormatter('es_Es', IntlDateFormatter::FULL,IntlDateFormatter::NONE);
 
 
-$formato->setPattern('EEEE, d \'de\', MMMM ,\'de\' y');
 
-echo ucfirst($formato->format($fecha));
+
+function esCapicua($numero){
+//convertimos el numero a cadena
+ $cadena = strval($numero);
+//cadena Invertida
+ $cadenaInvertida =   strrev($cadena);
+
+ //comparamos
+ if($cadena == $cadenaInvertida){
+   return true;
+ }else{
+    return false;
+ }
+}
+$numeroPrueba = 281128;
+
+if(esCapicua($numeroPrueba)){
+    print"El numero $numeroPrueba es capicua";
+
+}else{
+    print"El numero $numeroPrueba no es capicua";
+}
 
 
 ?>
+
+
+
 </body>
+
 </html>
