63f53f2ceefc1477d80da02e84f345ce948d765a
Adrián Peña Carnero <adrianpcarnero@gmail.com>
2024-10-02 13:29:07 +0200

    miercoles

--- DWEC/EJERCICIOS/EjerciciosJavascript/UD2E1.js
+++ DWEC/EJERCICIOS/EjerciciosJavascript/UD2E1.js
@@ -137,7 +137,7 @@ function dibujarTriangulo(n) {
         resultado[i] = fila;
     }
     return resultado;
-    
+
 }
 
 
@@ -213,15 +213,194 @@ que indica el tamaño del polígono.
  */
 
 
-function mostrarPloligono(){
+function mostrarPoligono(figura, tamano) {
 
-    
+    let resultado = figura(tamano);
+
+
+    let figuraTexto = '';
+    for (let i = 0; i < resultado.length; i++) {
+        figuraTexto += resultado[i] + '\n';
+    }
+
+
+    alert(figuraTexto);
 }
 
 
+// mostrarPoligono(dibujarTriangulo, 3);
+
+
+
+/**
+ * 7-Crea una función que reciba un número e imprima la tabla de multiplicar.
+Crea una función que solicite dos números entre 0 y 10, y que imprima las tablas de multiplicar
+entre los números indicados. Las tablas aparecerán desde el número más pequeño al mayor
+(aquí hay validaciones).
+Añade el código auxiliar necesario para probar la aplicación.
+ * 
+ */
+
+
+
+function tablaMultiplicar(n) {
+
+    for (let i = 0; i < 11; i++) {
+
+        console.log(n * i);
+
+    }
 
 
 
 
 
+
+
+
+
+}
+
+
+// function tablaMultiplicar2() {
+
+//     let numero1 = parseInt(prompt("Introduce un numero en el rango de 0 a 10"));
+//     let numero2 = parseInt(prompt("Introduce el segundo numero en el rango de 0 a 10"));
+
+//     //condiciones
+//     (isNaN(numero1) || isNaN(numero2) || numero1 < 0 || numero1 > 10 || numero2 < 0 || numero2 > 10) ? console.log("Los numeros deben estar entre 0 y 10") :
+
+//         (function () {
+
+//             let menor, mayor;
+
+
+//             (numero1 > numero2) ? (mayor = numero1, menor = numero2) : (menor = numero1, mayor = numero2);
+
+
+//             for (let i = menor; i <= mayor; i++) {
+//                 console.log(`Tabla del ${i}`);
+//                 for (let j = 0; j < 10; j++) {
+//                     console.log(`${i} x ${j}= ${i * j}`);
+
+//                 }
+//                 console.log(' ')
+//             }
+
+
+//         })();
+// }
+
+// tablaMultiplicar2();
+
+
+
+
+
+/**
+ * 
+ * 8-Crea una función que reciba un número y calcule su factorial, la función devolverá una
+cadena con el desarrollo del factorial. Por ejemplo, para una entrada de 4 devolverá la cadena
+“4x3x2x1=24”.
+Añade el código auxiliar necesario para probar la aplicación. El script solicitará números al
+usuario hasta que no desee continuar.
+ */
+
+
+// function factorialNumero(n) {
+
+//     let cadena = '';
+//     let resultado = 1;
+//     for (let i = n; i > 0; i--) {
+//         resultado *= i;
+//         cadena += i + (i > 1 ? "X" : "");
+
+
+
+//     }
+//     cadena += "=" + resultado;
+//     return cadena;
+// }
+
+
+
+
+// let resultado5 = factorialNumero(8);
+// console.log(resultado5);
+
+
+
+
+/**
+ * 9-Crea una función que reciba un número variable de parámetros y que los agrupe por tipo.
+Después por cada tipo se mostrará el tipo y la colección de parámetros de ese tipo incluyendo
+la posición original entre los parámetros.
+Añade el código auxiliar necesario para probar la aplicación.
+ */
+
+
+// function agrupacionTipo(...parametros) {
+//     const tipos = [];
+
+//     for (let i = 0; i < parametros.length; i++) {
+
+//         let tipo = typeof parametros[i];
+
+//         (!tipos[tipo]) ? ();
+
+//     }
+
+
+
+
+
+// }
+
+
+
+
+
+/**
+ * 
+ * 12-Juego acertar número.
+Calcula un numero aleatorio entre 1 y 100, este número permanecerá secreto. Pide al usuario
+un numero en rango, el programa le dirá si es mayor o menor que el número secreto. Si el
+usuario acierta el número secreto gana. El usuario dispondrá de 5 intentos para acertar el
+número secreto, tras lo cual, el programa dirá que ha perdido y le mostrará el número secreto.
+Añade el código auxiliar necesario para probar la aplicación
+ * 
+ * 
+ * 
+ */
+
+
+let numSecreto = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+
+
+
+while (intentos < 5) {
+
+
+    let respuesta = parseInt(prompt("Adivina el numero entre 1 y 100"));
+
+    if (respuesta === numSecreto) {
+        alert("Adivinaste el número");
+        break;
+    } else {
+        intentos++;
+        if (respuesta > numSecreto) {
+            alert("El numero es mas pequeño");
+        } else {
+            alert("El numero es mas grande");
+        }
+
+    }
+
+    if (intentos === 5) {
+       alert("Game Over");
+    }
+
+
+}
 
